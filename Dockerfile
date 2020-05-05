0d5b5cc694296b4a3453dfcad32e344194417d01
Fran <franciscojose.catala@gmail.com>
2020-05-05 16:55:36 +0200

    - Switched redis php session enablement to entrypoint based on env vars $REDIS_HOST $REDIS_PASSWORD - Added wordpress siteurl and host config in entrypoint via env vars $WORDPRESS_HOST $HTTP_PROTO

--- Dockerfile
+++ Dockerfile
@@ -5,13 +5,15 @@ ENV DEBIAN_FRONTEND noninteractive
 
 RUN apt update \
   && apt install apache2 libapache2-mod-php php-gd php-mysql php-xml php-intl php-redis wget ca-certificates -y --no-install-recommends \
-  && sed -i 's/^session.save_handler = files/session.save_handler = redis\n session.save_path = "tcp:\/\/redis:6379"/g' /etc/php/7.3/apache2/php.ini \
   && rm -f /var/www/html/index.html
 RUN   wget -O- https://wordpress.org/latest.tar.gz | tar --strip-components=1 -C /var/www/html/ -zxv \
   && rm -rf /var/lib/apt/lists/* 
 
-#WORKDIR /var/www/html
 COPY entrypoint.sh /entrypoint.sh
+
+WORKDIR /var/www/html
+
+COPY sess_test.php .
 
 EXPOSE 80
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -11,9 +11,24 @@ if [ ! -f /var/www/html/wp-config.php ]; then
       echo s/DB_HOST\', \'localhost/DB_HOST\', \'${MYSQL_HOST}/g;
       echo s/DB_NAME\', \'\'/DB_NAME\', \'${MYSQL_DATABASE}\'/;
     } | sed -f - /var/www/html/wp-config-sample.php > /var/www/html/wp-config.php;
+    {
+      echo "define('WP_HOME','${HTTP_PROTO:-http}://${WORDPRESS_HOST:-localhost}');";
+      echo "define('WP_SITEURL','${HTTP_PROTO:-http}://${WORDPRESS_HOST:-localhost}');";
+    } >> /var/www/html/wp-config.php
   fi
 else
-  echo "NOTICE: Config file exists. Continuing..."
+  echo "NOTICE: Config file exists. Continuing with HOST FQDN reconfig..."
+  {
+    echo s/WP_HOME\',\'.*\'/WP_HOME\',\'${HTTP_PROTO:-http}://${WORDPRESS_HOST:-localhost}\'/g;
+    echo s/WP_SITEURL\',\'.*\'/WP_SITEURL\',\'${HTTP_PROTO:-http}://${WORDPRESS_HOST:-localhost}\'/g;
+  } | sed -f - -i /var/www/html/wp-config.php
+fi
+
+if [ ! -z "${REDIS_HOST}" ]; then
+  echo "NOTICE: Setting up Redis session handler..."
+  {
+    echo s/^session.save_handler = files/session.save_handler = redis\\n session.save_path = "tcp:\/\/${REDIS_HOST:-redis}:${REDIS_PORT:-6379}\/?auth=${REDIS_PASSWORD:-}"/g;
+  } | sed -f - -i /etc/php/7.3/apache2/php.ini
 fi
 
 apachectl -D FOREGROUND
